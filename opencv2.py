b2d3904ad1f8a1843c28e0a69a40ea0bcc5e8907
ibraheem101 <ayokeks@gmail.com>
2022-11-17 03:21:06 +0100

    Added object detection in HSV

--- opencv2.py
+++ opencv2.py
@@ -9,21 +9,56 @@ import matplotlib.pyplot as plt
 def nothing(x):
     pass
 
+# cv2.namedWindow('frame')
+# cv2.createTrackbar('test', 'frame', 0, 100, nothing)
+
+# capture = cv2.VideoCapture(0)
+# while True:
+#     isTrue, frame = capture.read()
+
+#     pos = cv2.getTrackbarPos('test', 'frame')
+#     cv2.putText(frame, str(pos), (19, 50), cv2.FONT_HERSHEY_COMPLEX, 3, (0,255,0)), 
+
+#     # cv2.imshow('frame', frame)
+
+    # if cv2.waitKey(20) & 0xFF == ord('q'):
+    #     break
+
+# capture.release()
+# cv2.destroyAllWindows()
+
+#### Object detection using HSV Color space and trackbars
+
 cv2.namedWindow('frame')
-cv2.createTrackbar('test', 'frame', 0, 100, nothing)
+cv2.createTrackbar('H', 'frame', 0, 180, nothing)
+cv2.createTrackbar('S', 'frame', 0, 255, nothing)
+cv2.createTrackbar('V', 'frame', 0, 255, nothing)
 
-capture = cv2.VideoCapture(0)
+
+
+capture2 = cv2.VideoCapture(0)
 while True:
-    isTrue, frame = capture.read()
-
-    pos = cv2.getTrackbarPos('test', 'frame')
-    cv2.putText(frame, str(pos), (19, 50), cv2.FONT_HERSHEY_COMPLEX, 3, (0,255,0)), 
-
+    isTrue, frame = capture2.read()
     cv2.imshow('frame', frame)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #Let's 
+    h_position = cv2.getTrackbarPos('H', 'frame')
+    s_position = cv2.getTrackbarPos('S', 'frame')
+    v_position = cv2.getTrackbarPos('V', 'frame')
+    hsv_values = np.array([h_position, s_position, v_position])
+
+    lower = np.array([h_position, 0, 0])
+    upper = hsv_values
+    mask = cv2.inRange(hsv, lower, upper)
+    
+    # cv2.imshow('hsv', hsv)
+    cv2.imshow('mask', mask)
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
+capture2.release()
+capture2.destroyAllWindows()
 
-capture.release()
-cv2.destroyAllWindows()
+
 cv2.waitKey()
