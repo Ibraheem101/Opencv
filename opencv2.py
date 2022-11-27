904448cb909ed8914215af0e787ff18bff118f63
ibraheem101 <ayokeks@gmail.com>
2022-11-27 23:03:48 +0100

    Addes perspective transformation

--- opencv2.py
+++ opencv2.py
@@ -29,37 +29,64 @@ def nothing(x):
 
 #### Object detection using HSV Color space and trackbars
 
-cv2.namedWindow('frame')
-cv2.createTrackbar('H', 'frame', 0, 180, nothing)
-cv2.createTrackbar('S', 'frame', 0, 255, nothing)
-cv2.createTrackbar('V', 'frame', 0, 255, nothing)
+# cv2.namedWindow('frame')
+# cv2.createTrackbar('H', 'frame', 0, 180, nothing)
+# cv2.createTrackbar('S', 'frame', 0, 255, nothing)
+# cv2.createTrackbar('V', 'frame', 0, 255, nothing)
 
 
 
-capture2 = cv2.VideoCapture(0)
-while True:
-    isTrue, frame = capture2.read()
-    cv2.imshow('frame', frame)
+# capture2 = cv2.VideoCapture(0)
+# while True:
+#     isTrue, frame = capture2.read()
+#     # cv2.imshow('frame', frame)
 
-    blur = cv2.GaussianBlur(frame, (5, 5), cv2.BORDER_DEFAULT)
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    #Let's 
-    h_position = cv2.getTrackbarPos('H', 'frame')
-    s_position = cv2.getTrackbarPos('S', 'frame')
-    v_position = cv2.getTrackbarPos('V', 'frame')
-    hsv_values = np.array([h_position, s_position, v_position])
+#     blur = cv2.GaussianBlur(frame, (5, 5), cv2.BORDER_DEFAULT)
+#     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+#     #Let's 
+#     h_position = cv2.getTrackbarPos('H', 'frame')
+#     s_position = cv2.getTrackbarPos('S', 'frame')
+#     v_position = cv2.getTrackbarPos('V', 'frame')
+#     hsv_values = np.array([h_position, s_position, v_position])
 
-    lower = np.array([h_position, 0, 0])
-    upper = hsv_values
-    mask = cv2.inRange(hsv, lower, upper)
+#     lower = np.array([h_position, 0, 0])
+#     upper = hsv_values
+#     mask = cv2.inRange(hsv, lower, upper)
     
-    # cv2.imshow('hsv', hsv)
-    cv2.imshow('mask', mask)
+#     # cv2.imshow('hsv', hsv)
+#     # cv2.imshow('mask', mask)
 
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-capture2.release()
-capture2.destroyAllWindows()
+#     if cv2.waitKey(20) & 0xFF == ord('q'):
+#         break
+# capture2.release()
+# capture2.destroyAllWindows()
+
+
+
+#### Perspective Transformations
+perspective = cv2.imread("C:/Users/User/OneDrive/Pictures/perspective.png")
+cv2.circle(perspective, (191, 17), 4, (0, 0, 255), -1)
+cv2.circle(perspective, (207, 17), 4, (0, 0, 255), -1)
+cv2.circle(perspective, (16, 126), 4, (0, 0, 255), -1)
+cv2.circle(perspective, (380, 126), 4, (0, 0, 255), -1)
+
+pts1 = np.float32([[191, 17], [207, 17], [16, 126], [380, 126]])
+pts2 = np.float32([[0, 0], [400, 0], [0, 700], [400, 700]])
+
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+result = cv2.warpPerspective(perspective, matrix, (400, 700))
+
+cv2.imshow('Result', result)
+
+print(perspective.shape)
+
+
+
+
+
+
+
+
 
 
 cv2.waitKey()
